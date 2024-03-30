4bb50551b2e62d66c6eb0367bbf623e9ffa53fb3
Guilherme Nisiyama <guilherme_nisiyama@gmail.com>
2024-03-30 17:40:46 -0300

    Commit Program 04

---
+++ Program04/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Program04;
+
+class Program()
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please type some text and press Enter");
+        string phrase = Console.ReadLine();
+
+        Console.WriteLine("Please type the letter you want to count and press Enter");
+        string letter = Console.ReadLine();
+
+
+        int count_letters = 0;
+
+        foreach (char i in phrase) {           
+            if (i.ToString().ToLower() == letter.ToLower()) {  count_letters++; }
+        }
+
+        Console.WriteLine($"Quantidade de letras: {count_letters}");
+    }
+}
